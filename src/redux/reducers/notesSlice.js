4c765a751ac564ec2bf1bb4ac74d5fa51e81f4f7
Alex <alexballen53@gmail.com>
2023-06-11 17:41:46 -0500

    Se configura la estructura del reducer slice de notes

---
+++ src/redux/reducers/notesSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const notesSlice = createSlice({
+  name: "notes",
+  initialState,
+  reducers: {},
+});
+
+export const {} = notesSlice.actions;
+
+export default notesSlice.reducer;
