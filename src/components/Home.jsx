77e7ec1b7d55a996cf176b91676ea3dc6e495c85
Alex <alexballen53@gmail.com>
2023-06-12 18:04:09 -0500

    Se crea el componente Home y se configura su estructura basica y su archivo module.css

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import s from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <main>
+      <div className={s.container}>
+        <h1>home</h1>
+        <h3>Bienvenidos al home la intro e informacion de la App</h3>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
