1ef59e7b15e897810114b35429462ced3baf4f01
Alex <alexballen53@gmail.com>
2023-06-12 18:04:56 -0500

    Se crea el componente NavBar y se configura su estructura basica y su archivo module.css

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import s from "./NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <main>
+      <div className={s.container}>
+        <section>
+          <nav>
+            <div className={s.nav_container}>
+              <div className={s.home_container}>
+                <a className={s.nav_link} href="/">
+                  Home
+                </a>
+              </div>
+              <div className={s.signup_container}>
+                <a className={s.nav_link} href="/signup">
+                  SignUp
+                </a>
+              </div>
+              <div className={s.signin_container}>
+                <a className={s.nav_link} href="/signin">
+                  SignIn
+                </a>
+              </div>
+            </div>
+          </nav>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default NavBar;
