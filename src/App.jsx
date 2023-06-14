dde7a3a1e25e2d4007138bf0449f475376bb52ba
Alex <alexballen53@gmail.com>
2023-06-14 14:15:41 -0500

    Se crea la proteccion de rutas en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import ProtectRoutes from "./services/ProtectRoutes";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import NavBar from "./components/NavBar";
-
 import "./App.css";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(true);
+  const [isAppLoaded, setIsAppLoaded] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsAuth(true);
+    } else {
+      setIsAuth(false);
+    }
+    setIsAppLoaded(true);
+  }, []);
+
+  const pathSignIn = "/signin";
+  const protectedHome = ProtectRoutes(isAuth, Home, pathSignIn);
+
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
+      {isAppLoaded && (
+        <Routes>
+          <Route exact path="/" element={protectedHome} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      )}
     </>
   );
 }
