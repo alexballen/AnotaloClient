5cee9a7dbe1330d1f4f40c60e4845df205996f44
Alex <alexballen53@gmail.com>
2023-06-13 20:39:16 -0500

    Se añade state al store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import allUsers from "./reducers/userSlice.js";
+import token from "./reducers/userSlice.js";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    allUsers,
+    token,
+  },
 });
