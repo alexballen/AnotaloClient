01ef21269eb5c4e53b8223fb33243c1949853fc7
Alex <alexballen53@gmail.com>
2023-06-15 18:35:31 -0500

    Se modifican action de redux, la action postSignInGoogle, postGoogle

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -19,7 +19,11 @@ export const getDbUsers = () => async (dispatch) => {
 export const postSignUp = (dataUser) => async (dispatch) => {
   try {
     const addUser = await axios.post(`${baseURL}/signup`, dataUser);
-    dispatch(addDbUser(addUser.data));
+    dispatch(addDbUser(addUser.data.createUser));
+    console.log(addUser.data.message);
+    if (addUser.data.message === "Registro exitoso") {
+      alert("Registro exitoso");
+    }
   } catch (error) {
     if (error.response) {
       console.log(error.response.data.error);
@@ -32,12 +36,43 @@ export const postSignUp = (dataUser) => async (dispatch) => {
 export const postSignIn = (dataUser) => async (dispatch) => {
   try {
     const userAuth = await axios.post(`${baseURL}/signin`, dataUser);
-    dispatch(authToken(userAuth.data));
+    dispatch(authToken(userAuth.data.token));
+    console.log(userAuth.data.message);
+    if (userAuth.data.message === "Inicio de sesion exitoso") {
+      alert("Inicio de sesion exitoso");
+    }
   } catch (error) {
     if (error.response) {
       console.log(error.response.data.error);
     } else {
       console.log(error.message);
     }
+  }
+};
+
+export const postSignInGoogle = () => async () => {
+  try {
+    const userAuthGoogle = await axios.get(`${baseURL}/auth/google`);
+    console.log(userAuthGoogle.data);
+    const authorizationUrl = userAuthGoogle.data;
+    window.location.href = authorizationUrl;
+
+    /* const getToken = await axios.get(`${baseURL}/auth/google/callback`);
+    console.log(getToken); */
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postGoogle = (urlData) => async () => {
+  console.log(urlData);
+  try {
+    const portCodeGogle = await axios.post(
+      `${baseURL}/auth/google/callback`,
+      urlData
+    );
+    console.log(portCodeGogle.data);
+  } catch (error) {
+    console.log(error);
   }
 };
