54cc623b58a05ee15a585166cc50e86594041e99
Alex <alexballen53@gmail.com>
2023-06-15 18:36:20 -0500

    Se crean la rutas para la autenticacion de google

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import NavBar from "./components/NavBar";
+import SignInGoogle from "./components/SignInGoogle";
+import GoogleCallbak from "./components/GoogleCallbak";
 import "./App.css";
 
 function App() {
@@ -24,12 +26,28 @@ function App() {
   const pathSignIn = "/signin";
   const protectedHome = ProtectRoutes(isAuth, Home, pathSignIn);
 
+  /* useEffect(() => {
+    // Verificar si la URL contiene el código de retorno de Google
+    if (location.pathname === "/auth/google/callback") {
+      const code = new URLSearchParams(location.search).get("code");
+      console.log(code);
+
+      // Realizar las acciones necesarias con el código de retorno de Google
+      // Por ejemplo, enviarlo al servidor para obtener el token de acceso
+
+      // Redirigir al usuario a la página de inicio después de manejar la respuesta de Google
+      window.location.href = "/";
+    }
+  }, [location]); */
+
   return (
     <>
       <NavBar />
       {isAppLoaded && (
         <Routes>
           <Route exact path="/" element={protectedHome} />
+          <Route path="/auth/google" element={<SignInGoogle />} />
+          <Route path="/auth/google/callback" element={<GoogleCallbak />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
         </Routes>
