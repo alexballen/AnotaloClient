29e4aa707f8cb638ad66e23129e9702e9e088bdc
Alex <alexballen53@gmail.com>
2023-06-16 19:29:45 -0500

    Se modifican el componente GoogleCallback para merjorar su funcionamiento

--- src/components/GoogleCallbak.jsx
+++ src/components/GoogleCallbak.jsx
@@ -1,28 +1,105 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { postGoogle } from "../redux/actions/users.js";
+/* import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { postSignInGoogle } from "../redux/actions/users.js";
+import { decodedToken } from "../services/services.js";
+import Home from "./Home.jsx";
+import SignIn from "./SignIn.jsx";
 
 const GoogleCallback = () => {
   const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.token);
+
+  decodedToken(token);
+
   const [currentURL, setCurrentURL] = useState("");
 
   useEffect(() => {
     setCurrentURL(window.location.href);
-  }, []);
+  }, []); */
 
-  if (currentURL.length) {
-    const url = new URL(currentURL);
-    const params = new URLSearchParams(url.search);
-    const code = params.get("code");
+/* useEffect(() => {
+    const handleGoogleSignIn = async () => {
+      if (currentURL.length) {
+        const url = new URL(currentURL);
+        const params = new URLSearchParams(url.search);
+        const googleAuthorizationCode = params.get("code");
 
-    dispatch(postGoogle(code));
-  }
+        await dispatch(postSignInGoogle(googleAuthorizationCode));
 
-  return (
-    <div>
-      <p>Current URL</p>
-    </div>
-  );
+        if (Object.keys(token).length) {
+          window.location.href = "/";
+        } else {
+          window.location.href = "/signin";
+        }
+      }
+    };
+
+    handleGoogleSignIn();
+  }, [currentURL, dispatch, token]); */
+
+/* useEffect(() => {
+    if (currentURL.length) {
+      const url = new URL(currentURL);
+      const params = new URLSearchParams(url.search);
+      const googleAuthorizationCode = params.get("code");
+
+      if (googleAuthorizationCode && !Object.keys(token).length) {
+        dispatch(postSignInGoogle(googleAuthorizationCode));
+      }
+    }
+  }, [currentURL, dispatch, token]);
+
+  useEffect(() => {
+    if (Object.keys(token).length) {
+      window.location.href = "/";
+    } else {
+      window.location.href = "/signin";
+    }
+  }, [token]);
+
+  return <>{Object.keys(token).length ? <Home /> : <SignIn />}</>;
+};
+
+export default GoogleCallback; */
+
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { postSignInGoogle } from "../redux/actions/users.js";
+import { decodedToken } from "../services/services.js";
+import { useLocation, useNavigate } from "react-router-dom";
+
+/* import Home from "./Home.jsx";
+import SignIn from "./SignIn.jsx"; */
+
+const GoogleCallback = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.token);
+
+  decodedToken(token);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [currentURL, setCurrentURL] = useState("");
+
+  useEffect(() => {
+    setCurrentURL(location.search);
+  }, [location]);
+
+  useEffect(() => {
+    if (currentURL.length) {
+      const params = new URLSearchParams(currentURL);
+      const googleAuthorizationCode = params.get("code");
+
+      dispatch(postSignInGoogle(googleAuthorizationCode));
+    }
+  }, [currentURL, dispatch]);
+
+  /*  useEffect(() => {
+    if (Object.keys(token).length) {
+      return navigate("/");
+    }
+  }, [token, navigate]); */
+  return Object.keys(token).length ? navigate("/") : null;
 };
 
 export default GoogleCallback;
