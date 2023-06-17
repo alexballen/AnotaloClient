b369fb134ba3b7db4a9c459031af18be2ac5b101
Alex <alexballen53@gmail.com>
2023-06-16 19:28:41 -0500

    Se elimina codigo obsoleto del componente App

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,6 @@ function App() {
   const pathSignIn = "/signin";
   const protectedHome = ProtectRoutes(isAuth, Home, pathSignIn);
 
-  /* useEffect(() => {
-    // Verificar si la URL contiene el código de retorno de Google
-    if (location.pathname === "/auth/google/callback") {
-      const code = new URLSearchParams(location.search).get("code");
-      console.log(code);
-
-      // Realizar las acciones necesarias con el código de retorno de Google
-      // Por ejemplo, enviarlo al servidor para obtener el token de acceso
-
-      // Redirigir al usuario a la página de inicio después de manejar la respuesta de Google
-      window.location.href = "/";
-    }
-  }, [location]); */
-
   return (
     <>
       <NavBar />
