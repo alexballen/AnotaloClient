c6ff3f6a1ef8ad42856e30788c4b5b2edc980de0
Alex <alexballen53@gmail.com>
2023-06-17 20:38:36 -0500

    Se crea el component de inicio de sesion de google

---
+++ src/components/SignInGoogle.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { FcGoogle } from "react-icons/fc";
+import { getSignInGoogle } from "../redux/actions/users";
+import s from "./SignUp.module.css";
+
+const SignInGoogle = () => {
+  const dispatch = useDispatch();
+
+  const handleAuthGoogle = () => {
+    dispatch(getSignInGoogle());
+  };
+
+  return (
+    <div>
+      <section>
+        <div className={s.signin_google_container}>
+          <button className={s.signin_button} onClick={handleAuthGoogle}>
+            <FcGoogle />
+            Sign In with Google
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SignInGoogle;
