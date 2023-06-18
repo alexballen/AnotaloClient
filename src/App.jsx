8d502237eeb5b2eeb4ef4238a1e1a70a677bedec
Alex <alexballen53@gmail.com>
2023-06-17 20:14:28 -0500

    Mejoras

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,47 @@
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import { saveToken } from "./redux/actions/users";
 import ProtectRoutes from "./services/ProtectRoutes";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import NavBar from "./components/NavBar";
 import SignInGoogle from "./components/SignInGoogle";
-import GoogleCallbak from "./components/GoogleCallbak";
+import GoogleCallback from "./components/GoogleCallback";
+import Notes from "./components/Notes";
 import "./App.css";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(true);
+  const dispatch = useDispatch();
+  const [isAuth, setIsAuth] = useState(false);
   const [isAppLoaded, setIsAppLoaded] = useState(false);
 
+  const { token } = useSelector((state) => state.token);
+  console.log(token);
+
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    dispatch(saveToken());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (token) {
       setIsAuth(true);
-    } else {
-      setIsAuth(false);
     }
     setIsAppLoaded(true);
-  }, []);
+  }, [token]);
 
-  const pathSignIn = "/signin";
-  const protectedHome = ProtectRoutes(isAuth, Home, pathSignIn);
+  const protectedHome = ProtectRoutes(isAuth, Home, "/signin");
 
   return (
     <>
       <NavBar />
       {isAppLoaded && (
         <Routes>
-          <Route exact path="/" element={protectedHome} />
+          <Route path="/auth/google/callback" element={<GoogleCallback />} />
+          <Route path="/home" element={protectedHome} />
+          <Route path="/notes" element={<Notes />} />
           <Route path="/auth/google" element={<SignInGoogle />} />
-          <Route path="/auth/google/callback" element={<GoogleCallbak />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
         </Routes>
