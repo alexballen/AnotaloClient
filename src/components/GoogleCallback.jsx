7ec07ee16e939c751a26e378f6d5aee97d8494ed
Alex <alexballen53@gmail.com>
2023-06-19 17:59:08 -0500

    Se solucionan bug de inicio de sesion con google para que redirija a la pagina que luego de recibir las credenciales validas, tambien que no precargue al SignIn antes de redirigir a /Home

--- src/components/GoogleCallback.jsx
+++ src/components/GoogleCallback.jsx
@@ -1,32 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postSignInGoogle, saveToken } from "../redux/actions/users.js";
-import { useLocation, useNavigate } from "react-router-dom";
+import { postSignInGoogle } from "../redux/actions/users.js";
+import { useLocation } from "react-router-dom";
+import SignIn from "./SignIn.jsx";
+import Home from "./Home.jsx";
 
 const GoogleCallback = () => {
   const dispatch = useDispatch();
   const location = useLocation();
-  const navigate = useNavigate();
 
   const { token } = useSelector((state) => state.token);
-  console.log(token);
+
+  const [redirectToHome, setRedirectToHome] = useState(false);
 
   const params = new URLSearchParams(location.search);
   const googleAuthorizationCode = params.get("code");
-  console.log(googleAuthorizationCode);
+  const googleVerificationCode = {
+    googleAuthorizationCode,
+  };
 
   useEffect(() => {
     if (googleAuthorizationCode) {
-      dispatch(postSignInGoogle(googleAuthorizationCode));
-      /* navigate("/home"); */
+      dispatch(postSignInGoogle(googleVerificationCode));
+      setRedirectToHome(true);
     }
-  }, [dispatch, googleAuthorizationCode, navigate]);
+  }, []);
 
   useEffect(() => {
-    dispatch(saveToken());
-  }, [dispatch]);
+    if (googleAuthorizationCode) {
+      const timer = setTimeout(() => {}, 1000);
 
-  return null;
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
+  if (redirectToHome && Object.keys(token).length) {
+    return <Home />;
+  } else if (googleAuthorizationCode) {
+    return <div>Cargando...</div>;
+  } else {
+    return <SignIn />;
+  }
 };
 
 export default GoogleCallback;
