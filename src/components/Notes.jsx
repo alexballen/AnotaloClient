7c19f333f3534d366565a0d60170b395babfa567
Alex <alexballen53@gmail.com>
2023-06-20 20:33:07 -0500

    Se configura la logica del componente Notes para que decodifique el token en el backend lo devuelva decodificado

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { saveToken, validateToken } from "../redux/actions/users";
+import { getDbNotes } from "../redux/actions/notes";
+import s from "./Notes.module.css";
 
 const Notes = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.token);
+  console.log(token);
+
+  const { decoded } = useSelector((state) => state.decoded);
+  console.log(decoded);
+
+  const { allNotes } = useSelector((state) => state.allNotes);
+  console.log(allNotes);
+
+  useEffect(() => {
+    dispatch(saveToken());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (Object.keys(token).length) {
+      const objToken = {
+        token,
+      };
+      dispatch(validateToken(objToken));
+    }
+  }, [token]);
+
+  useEffect(() => {
+    dispatch(getDbNotes());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Notes</h1>
-    </div>
+    <main>
+      <div className={s.container}>
+        <h1>Notes</h1>
+      </div>
+    </main>
   );
 };
 
