071b5aa431a48a91392e0e599af982e7071ec972
Alex <alexballen53@gmail.com>
2023-06-22 11:07:51 -0500

    Se crean los handles del componente Notes que manjenan la solicitud, la eliminacion y edicion de las notas hacia el backend

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,19 +1,22 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { saveToken, validateToken } from "../redux/actions/users";
-import { getDbNotes } from "../redux/actions/notes";
+import {
+  getDbNotes,
+  createNoteDb,
+  deleteNoteDb,
+  editNoteDb,
+} from "../redux/actions/notes";
 import s from "./Notes.module.css";
 
 const Notes = () => {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.token);
-  console.log(token);
 
   const { decoded } = useSelector((state) => state.decoded);
-  console.log(decoded);
 
   const { allNotes } = useSelector((state) => state.allNotes);
-  console.log(allNotes);
+  const objNotes = allNotes[0]?.notes;
 
   useEffect(() => {
     dispatch(saveToken());
@@ -29,13 +32,70 @@ const Notes = () => {
   }, [token]);
 
   useEffect(() => {
-    dispatch(getDbNotes());
-  }, [dispatch]);
+    if (decoded.email) {
+      const searchNotesUser = {
+        email: decoded.email,
+      };
+      dispatch(getDbNotes(searchNotesUser));
+    }
+  }, [dispatch, decoded]);
+
+  const objCreateNote = {
+    name: "Nota nueva 1",
+    description: "Nota de prueba de envio de solicitud http",
+    importance: "high",
+  };
+
+  const idUser = decoded.id;
+
+  const handleCreateNote = () => {
+    dispatch(createNoteDb(idUser, objCreateNote));
+  };
+
+  const handleDeleteNote = (idNote) => {
+    dispatch(deleteNoteDb(idNote));
+  };
+
+  const objEditNote = {
+    name: "Nota # 4",
+    description: "Nota de Cumpleaños Josuha",
+    importance: "medium",
+  };
+
+  const handleEditNote = (idNote) => {
+    dispatch(editNoteDb(idNote, objEditNote));
+  };
 
   return (
     <main>
       <div className={s.container}>
         <h1>Notes</h1>
+        <div>
+          <button onClick={handleCreateNote}>Crear Nota Nueva</button>
+        </div>
+        {objNotes?.map((notes, index) => {
+          return (
+            <div key={index}>
+              <div>
+                <p>ID: {notes.id}</p>
+              </div>
+              <div>
+                <p>NAME: {notes.name}</p>
+              </div>
+              <div>
+                <p>DESCRIPTION: {notes.description}</p>
+              </div>
+              <div>
+                <p>IMPORTANCE: {notes.importance}</p>
+              </div>
+              <button onClick={() => handleDeleteNote(notes.id)}>Delete</button>
+              <button onClick={() => handleEditNote(notes.id)}>
+                Editar Nota
+              </button>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     </main>
   );
