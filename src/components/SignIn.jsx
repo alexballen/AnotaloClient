fb8b1d04ac5fdff62a8b18c58ff06e30068154e9
Alex <alexballen53@gmail.com>
2023-06-22 11:09:05 -0500

    Se llama al estado token y se crea la redireccion si hay token correcto

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { postSignIn } from "../redux/actions/users.js";
 import { handleSubmit, handleChange } from "../services/services.js";
-
+import { useNavigate } from "react-router-dom";
 import s from "./SignIn.module.css";
 
 const SignIn = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { token } = useSelector((state) => state.token);
 
   const [signInData, setSignInData] = useState({
     email: "",
     password: "",
   });
+
+  useEffect(() => {
+    if (token.length) {
+      navigate("/notes");
+    }
+  }, [token, navigate]);
 
   return (
     <main>
