fd0b832c66a4b0f071d30a7f9f4fdf820308c018
Alex <alexballen53@gmail.com>
2023-06-22 11:10:19 -0500

    Se crean los estados allNotes y decoded en el store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import allUsers from "./reducers/userSlice.js";
 import token from "./reducers/userSlice.js";
+import allNotes from "./reducers/notesSlice.js";
+import decoded from "./reducers/userSlice.js";
 
 export const store = configureStore({
   reducer: {
     allUsers,
     token,
+    allNotes,
+    decoded,
   },
 });
