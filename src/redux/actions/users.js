8c0d74690680ae3513153f9cc708af641fbcee94
Alex <alexballen53@gmail.com>
2023-06-22 11:58:47 -0500

    Se añaden a la creacion de los valores de autenticacion de los localStorage, se cambia la ruta del callback por signin y se crea la funcion de validacion del token en el backend

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { allDbUsers, addDbUser, authToken } from "../reducers/userSlice.js";
+import {
+  allDbUsers,
+  addDbUser,
+  authToken,
+  decodedToken,
+} from "../reducers/userSlice.js";
 
 const baseURL = "http://localhost:3001";
 
@@ -36,11 +41,16 @@ export const postSignUp = (dataUser) => async (dispatch) => {
 export const postSignIn = (dataUser) => async (dispatch) => {
   try {
     const userAuth = await axios.post(`${baseURL}/signin`, dataUser);
+
+    localStorage.setItem("token", userAuth.data.token);
+
+    localStorage.setItem("session", true);
+
     dispatch(authToken(userAuth.data.token));
 
-    if (userAuth.data.message === "Inicio de sesion exitoso") {
+    /*  if (userAuth.data.message === "Inicio de sesion exitoso") {
       alert("Inicio de sesion exitoso");
-    }
+    } */
   } catch (error) {
     if (error.response) {
       console.log(error.response.data.error);
@@ -60,22 +70,22 @@ export const getSignInGoogle = () => async () => {
   }
 };
 
-export const postSignInGoogle = (googleAuthorizationCode) => async () => {
-  try {
-    const googleAuthorizationToken = await axios.post(
-      `${baseURL}/auth/google/callback`,
-      googleAuthorizationCode
-    );
-    console.log(googleAuthorizationToken.data);
+export const postSignInGoogle =
+  (googleAuthorizationCode) => async (dispatch) => {
+    try {
+      const googleAuthorizationToken = await axios.post(
+        `${baseURL}/signin`,
+        googleAuthorizationCode
+      );
+      localStorage.setItem("token", googleAuthorizationToken.data);
 
-    localStorage.setItem(
-      "token",
-      JSON.stringify(googleAuthorizationToken.data)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
+      localStorage.setItem("session", true);
+
+      dispatch(authToken(googleAuthorizationToken.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 export const saveToken = () => (dispatch) => {
   try {
@@ -83,6 +93,15 @@ export const saveToken = () => (dispatch) => {
     if (token) {
       dispatch(authToken(token));
     }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const validateToken = (dataToken) => async (dispatch) => {
+  try {
+    const decodeToken = await axios.post(`${baseURL}/verification`, dataToken);
+    dispatch(decodedToken(decodeToken.data));
   } catch (error) {
     console.log(error);
   }
