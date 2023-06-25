f5c5f3d5a6faecf9e298c2214f6744e9011c3d8b
Alex <alexballen53@gmail.com>
2023-06-25 17:34:50 -0500

    Se crea la logica para que guarde los cambios efectuados en el componente AddNote al momento que si ingresan datos y no los guardan con las opcion crear nota nueva y le dan a la flecha de devolverser del navegador o le dan click a otra opcion de la barra de navegacion por decir a inicio, guarde los cambios automaticamente

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { validateToken } from "../redux/actions/users";
 import { createNoteDb } from "../redux/actions/notes";
-import { closingPopUp } from "../redux/actions/popUp";
 import s from "./AddNote.module.css";
 
-const AddNote = ({ idUser }) => {
+const AddNote = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { popUpClose } = useSelector((state) => state.popUpClose);
+  const { token } = useSelector((state) => state.token);
+  const { decoded } = useSelector((state) => state.decoded);
+
+  useEffect(() => {
+    if (Object.keys(token).length) {
+      const objToken = {
+        token,
+      };
+      dispatch(validateToken(objToken));
+    }
+  }, [dispatch, token]);
 
   const [note, setNote] = useState({
     name: "",
@@ -17,29 +29,70 @@ const AddNote = ({ idUser }) => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setNote((prevNote) => ({ ...prevNote, [name]: value }));
+    if ((name === "name" || name === "description") && !note.importance) {
+      setNote((prevNote) => ({
+        ...prevNote,
+        [name]: value,
+        importance: "medium",
+      }));
+    } else {
+      setNote((prevNote) => ({ ...prevNote, [name]: value }));
+    }
   };
 
+  useEffect(() => {
+    const handleLinkClick = (event) => {
+      event.preventDefault();
+
+      if (note.name.length > 0 || note.description.length > 0) {
+        handleCreateNote();
+      }
+
+      const path = event.target.getAttribute("href");
+      navigate(path);
+    };
+
+    const links = document.querySelectorAll("a");
+    links.forEach((link) => {
+      link.addEventListener("click", handleLinkClick);
+    });
+
+    return () => {
+      links.forEach((link) => {
+        link.removeEventListener("click", handleLinkClick);
+      });
+    };
+  }, [navigate, note]);
+
   const handleCreateNote = () => {
-    dispatch(createNoteDb(idUser, note));
+    if (note.name || note.description) {
+      dispatch(createNoteDb(decoded.id, note));
+    }
     setNote({
       name: "",
       description: "",
       importance: "",
     });
-    dispatch(closingPopUp(true));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  useEffect(() => {
+    const handlePopstate = () => {
+      if (note.name.length > 0 || note.description.length > 0) {
+        handleCreateNote();
+      }
+    };
+    window.addEventListener("popstate", handlePopstate);
+    return () => {
+      window.removeEventListener("popstate", handlePopstate);
+    };
+  }, [note]);
 
   return (
     <div>
       <main>
         <div className={s.container}>
           <section>
-            <form onSubmit={handleSubmit}>
+            <form>
               <div className={s.form_container}>
                 <div>
                   <input
@@ -70,7 +123,9 @@ const AddNote = ({ idUser }) => {
                 </div>
               </div>
               <div>
-                <button onClick={handleCreateNote}>Crear Nota Nueva</button>
+                <button type="button" onClick={handleCreateNote}>
+                  Crear Nota Nueva
+                </button>
               </div>
             </form>
           </section>
