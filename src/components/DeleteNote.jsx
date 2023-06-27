7fb24c9869a4e2f976847e4b2b887c0b59154f3c
Alex <alexballen53@gmail.com>
2023-06-26 19:54:40 -0500

    Se añade un redireccionamiento a la ruta notes, cuando se elimina una nota

--- src/components/DeleteNote.jsx
+++ src/components/DeleteNote.jsx
@@ -1,10 +1,13 @@
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { deleteNoteDb } from "../redux/actions/notes";
 
 const DeleteNote = ({ idNote }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleDeleteNote = () => {
+    navigate("/notes");
     dispatch(deleteNoteDb(idNote));
   };
 
