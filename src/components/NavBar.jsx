090d79dc681f601d5ea388624188850f254bb5f5
Alex <alexballen53@gmail.com>
2023-06-28 20:09:15 -0500

    Se crea la logica para que cuando haya inicio de sesion correcto aparezca en el navbar solo el logout, de lo contrario solo apareceria iniciar sesion y registrarse

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,57 @@
+import { useSelector } from "react-redux";
 import s from "./NavBar.module.css";
 
 const NavBar = () => {
+  const { token } = useSelector((state) => state.token);
+
   return (
     <main>
       <div className={s.container}>
         <section>
           <nav>
             <div className={s.nav_container}>
-              <div className={s.home_container}>
-                <a className={s.nav_link} href="/">
-                  Home
-                </a>
-              </div>
-              <div className={s.notes_container}>
-                <a className={s.nav_link} href="/notes">
-                  Notes
-                </a>
-              </div>
-              <div className={s.signup_container}>
-                <a className={s.nav_link} href="/signup">
-                  SignUp
-                </a>
-              </div>
-              <div className={s.signin_container}>
-                <a className={s.nav_link} href="/signin">
-                  SignIn
-                </a>
-              </div>
-              <div className={s.signin_container}>
-                <a className={s.nav_link} href="/logout">
-                  Logout
-                </a>
-              </div>
+              <section>
+                <div className={s.nav_der_container}>
+                  <div className={`${s.home_container} `}>
+                    <a className={s.nav_link} href="/">
+                      Inicio
+                    </a>
+                  </div>
+                  <div className={`${s.notes_container} `}>
+                    <a className={s.nav_link} href="/notes">
+                      Notas
+                    </a>
+                  </div>
+                </div>
+              </section>
+              <section>
+                <div className={s.nav_izq_container}>
+                  {token && Object.keys(token).length > 0 ? (
+                    <div className={s.logout_container}>
+                      <a className={s.nav_link} href="/logout">
+                        Cerrar Sesión
+                      </a>
+                    </div>
+                  ) : (
+                    <>
+                      {!Object.keys(token).length > 0 && (
+                        <div className={s.signin_container}>
+                          <a className={s.nav_link} href="/signin">
+                            Inicio Sesión
+                          </a>
+                        </div>
+                      )}
+                      {!Object.keys(token).length > 0 && (
+                        <div className={s.signin_container}>
+                          <a className={s.nav_link} href="/signup">
+                            Registrarse
+                          </a>
+                        </div>
+                      )}
+                    </>
+                  )}
+                </div>
+              </section>
             </div>
           </nav>
         </section>
