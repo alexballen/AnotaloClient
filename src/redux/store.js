91f31c5993317f05c006c570195cd882c01f4a18
Alex <alexballen53@gmail.com>
2023-06-28 20:14:11 -0500

    Se cambia el nombre del estado decodedToken para mejor claridad, se modifica el orden

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import allUsers from "./reducers/userSlice.js";
-import token from "./reducers/userSlice.js";
 import allNotes from "./reducers/notesSlice.js";
-import decoded from "./reducers/userSlice.js";
+import token from "./reducers/tokenSlice.js";
+import decodedToken from "./reducers/tokenSlice.js";
+import popUpClose from "./reducers/popUpSlice.js";
 
 export const store = configureStore({
   reducer: {
     allUsers,
-    token,
     allNotes,
-    decoded,
+    token,
+    decodedToken,
+    popUpClose,
   },
 });
