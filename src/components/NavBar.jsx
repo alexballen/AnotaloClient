4814b9fc328d93a20b83a6413fc244ea844afeac
Alex <alexballen53@gmail.com>
2023-06-29 22:55:05 -0500

    Se añaden elementos y logica al componente NavBar para crear efectos y estilos css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
+import { handleHover, handleMouseLeave } from "../services/services";
+import { FaUserLarge } from "react-icons/fa6";
 import s from "./NavBar.module.css";
 
 const NavBar = () => {
   const { token } = useSelector((state) => state.token);
+
+  const [isHoveredHome, setIsHoveredHome] = useState(false);
+  const [isHoveredNote, setIsHoveredNote] = useState(false);
 
   return (
     <main>
@@ -12,44 +18,71 @@ const NavBar = () => {
             <div className={s.nav_container}>
               <section>
                 <div className={s.nav_der_container}>
-                  <div className={`${s.home_container} `}>
-                    <a className={s.nav_link} href="/">
-                      Inicio
-                    </a>
+                  <div className={s.home_container}>
+                    <div className={s.line1}>
+                      <a
+                        className={s.nav_link}
+                        href="/"
+                        onMouseEnter={() => handleHover(setIsHoveredHome)}
+                        onMouseLeave={() => handleMouseLeave(setIsHoveredHome)}
+                      >
+                        Inicio
+                      </a>
+                    </div>
+                    <div className={s.line2}></div>
+                    <div
+                      className={`${s.line3} ${isHoveredHome ? s.rotated : ""}`}
+                    ></div>
                   </div>
-                  <div className={`${s.notes_container} `}>
-                    <a className={s.nav_link} href="/notes">
-                      Notas
-                    </a>
+                  <div className={s.notes_container}>
+                    <div className={s.line1}>
+                      <a
+                        className={s.nav_link}
+                        href="/notes"
+                        onMouseEnter={() => handleHover(setIsHoveredNote)}
+                        onMouseLeave={() => handleMouseLeave(setIsHoveredNote)}
+                      >
+                        Notas
+                      </a>
+                    </div>
+                    <div className={s.line2}></div>
+                    <div
+                      className={`${s.line3} ${isHoveredNote ? s.rotated : ""}`}
+                    ></div>
                   </div>
                 </div>
               </section>
               <section>
-                <div className={s.nav_izq_container}>
-                  {token && Object.keys(token).length > 0 ? (
-                    <div className={s.logout_container}>
-                      <a className={s.nav_link} href="/logout">
-                        Cerrar Sesión
-                      </a>
-                    </div>
-                  ) : (
-                    <>
-                      {!Object.keys(token).length > 0 && (
-                        <div className={s.signin_container}>
-                          <a className={s.nav_link} href="/signin">
-                            Inicio Sesión
-                          </a>
-                        </div>
-                      )}
-                      {!Object.keys(token).length > 0 && (
-                        <div className={s.signin_container}>
-                          <a className={s.nav_link} href="/signup">
-                            Registrarse
-                          </a>
-                        </div>
-                      )}
-                    </>
-                  )}
+                <div className={s.dropdown}>
+                  <button className={s.dropbtn}>
+                    <FaUserLarge />
+                  </button>
+                  <div className={s.dropdown_content}>
+                    {token && Object.keys(token).length > 0 ? (
+                      <div className={s.logout_container}>
+                        <a className={s.nav_link_sesion} href="/logout">
+                          Cerrar Sesión
+                        </a>
+                      </div>
+                    ) : (
+                      <>
+                        {!Object.keys(token).length > 0 && (
+                          <div className={s.signin_container}>
+                            <a className={s.nav_link_sesion} href="/signin">
+                              Inicio Sesión
+                            </a>
+                          </div>
+                        )}
+                        {!Object.keys(token).length > 0 && (
+                          <div className={s.signin_container}>
+                            <a className={s.nav_link_sesion} href="/signup">
+                              Registrarse
+                            </a>
+                          </div>
+                        )}
+                      </>
+                    )}
+                  </div>
                 </div>
               </section>
             </div>
