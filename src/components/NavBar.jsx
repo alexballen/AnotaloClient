ba939faa6e3b5abbc6cce8e7cfc0d0850e632249
Alex <alexballen53@gmail.com>
2023-07-01 19:40:45 -0500

    Se configura la logica del menu hamburguesa

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { handleHover, handleMouseLeave } from "../services/services";
 import { FaUserLarge } from "react-icons/fa6";
+import { AiOutlineMenu, AiOutlineMenuUnfold } from "react-icons/ai";
 import s from "./NavBar.module.css";
 
 const NavBar = () => {
@@ -9,6 +10,15 @@ const NavBar = () => {
 
   const [isHoveredHome, setIsHoveredHome] = useState(false);
   const [isHoveredNote, setIsHoveredNote] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <main>
@@ -17,75 +27,108 @@ const NavBar = () => {
           <nav>
             <div className={s.nav_container}>
               <section>
-                <div className={s.nav_der_container}>
-                  <div className={s.home_container}>
-                    <div className={s.line1}>
-                      <a
-                        className={s.nav_link}
-                        href="/"
-                        onMouseEnter={() => handleHover(setIsHoveredHome)}
-                        onMouseLeave={() => handleMouseLeave(setIsHoveredHome)}
-                      >
-                        Inicio
-                      </a>
-                    </div>
-                    <div className={s.line2}></div>
-                    <div
-                      className={`${s.line3} ${isHoveredHome ? s.rotated : ""}`}
-                    ></div>
-                  </div>
-                  <div className={s.notes_container}>
-                    <div className={s.line1}>
-                      <a
-                        className={s.nav_link}
-                        href="/notes"
-                        onMouseEnter={() => handleHover(setIsHoveredNote)}
-                        onMouseLeave={() => handleMouseLeave(setIsHoveredNote)}
-                      >
-                        Notas
-                      </a>
-                    </div>
-                    <div className={s.line2}></div>
-                    <div
-                      className={`${s.line3} ${isHoveredNote ? s.rotated : ""}`}
-                    ></div>
-                  </div>
-                </div>
-              </section>
-              <section>
-                <div className={s.dropdown}>
-                  <button className={s.dropbtn}>
-                    <FaUserLarge />
-                  </button>
-                  <div className={s.linedrop}></div>
-                  <div className={s.dropdown_content}>
-                    {token && Object.keys(token).length > 0 ? (
-                      <div className={s.logout_container}>
-                        <a className={s.nav_link_sesion} href="/logout">
-                          Cerrar Sesión
-                        </a>
-                      </div>
+                <figure>
+                  <div
+                    className={s.hamburguer__menu}
+                    onClick={() => toggleMenu()}
+                  >
+                    {isOpen ? (
+                      <AiOutlineMenuUnfold
+                        className={s.menu_close}
+                        onClick={closeMenu}
+                      />
                     ) : (
-                      <>
-                        {!Object.keys(token).length > 0 && (
-                          <div className={s.signin_container}>
-                            <a className={s.nav_link_sesion} href="/signin">
-                              Inicio Sesión
-                            </a>
-                          </div>
-                        )}
-                        {!Object.keys(token).length > 0 && (
-                          <div className={s.signin_container}>
-                            <a className={s.nav_link_sesion} href="/signup">
-                              Registrarse
-                            </a>
-                          </div>
-                        )}
-                      </>
+                      <AiOutlineMenu className={s.menu_open} />
                     )}
                   </div>
-                </div>
+                </figure>
               </section>
+              <div
+                className={`${s.items__menu__container} ${
+                  isOpen ? s.show : ""
+                }`}
+              >
+                <section>
+                  <div className={s.nav_der_container}>
+                    <div className={s.home_container}>
+                      <div className={s.line1}>
+                        <a
+                          className={s.nav_link}
+                          href="/"
+                          onMouseEnter={() => handleHover(setIsHoveredHome)}
+                          onMouseLeave={() =>
+                            handleMouseLeave(setIsHoveredHome)
+                          }
+                        >
+                          Inicio
+                        </a>
+                      </div>
+                      <div className={s.line2}></div>
+                      <div
+                        className={`${s.line3} ${
+                          isHoveredHome ? s.rotated : ""
+                        }`}
+                      ></div>
+                    </div>
+                    <div className={s.notes_container}>
+                      <div className={s.line1}>
+                        <a
+                          className={s.nav_link}
+                          href="/notes"
+                          onMouseEnter={() => handleHover(setIsHoveredNote)}
+                          onMouseLeave={() =>
+                            handleMouseLeave(setIsHoveredNote)
+                          }
+                        >
+                          Notas
+                        </a>
+                      </div>
+                      <div className={s.line2}></div>
+                      <div
+                        className={`${s.line3} ${
+                          isHoveredNote ? s.rotated : ""
+                        }`}
+                      ></div>
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div className={s.dropdown_container}>
+                    <div className={s.dropdown}>
+                      <button className={s.dropbtn}>
+                        <FaUserLarge />
+                      </button>
+                      <div className={s.linedrop}></div>
+                      <div className={s.dropdown_content}>
+                        {token && Object.keys(token).length > 0 ? (
+                          <div className={s.logout_container}>
+                            <a className={s.nav_link_sesion} href="/logout">
+                              Cerrar Sesión
+                            </a>
+                          </div>
+                        ) : (
+                          <>
+                            {!Object.keys(token).length > 0 && (
+                              <div className={s.signin_container}>
+                                <a className={s.nav_link_sesion} href="/signin">
+                                  Inicio Sesión
+                                </a>
+                              </div>
+                            )}
+                            {!Object.keys(token).length > 0 && (
+                              <div className={s.signin_container}>
+                                <a className={s.nav_link_sesion} href="/signup">
+                                  Registrarse
+                                </a>
+                              </div>
+                            )}
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
             </div>
           </nav>
         </section>
