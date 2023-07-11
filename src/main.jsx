5f4c1589bb1bb24f1857929434f77b7347aebd18
Alex <alexballen53@gmail.com>
2023-07-10 21:47:36 -0500

    Se crea el provider react-date-pickers para que se pueda acceder desde cualquier componente

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </LocalizationProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
